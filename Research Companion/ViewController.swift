f3082f79e77ffd071be4adbc2b6fca4e46296d08
Isioma Chuck <isioma.chuck@gmail.com>
2019-12-27 15:31:54 +0000

    Initial Commit

---
+++ Research Companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Research Companion
+//
+//  Created by Isioma Anofienam on 27/12/2019.
+//  Copyright © 2019 Isioma Anofienam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Research CompanionTests/Research_CompanionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Research_CompanionTests.swift
+//  Research CompanionTests
+//
+//  Created by Isioma Anofienam on 27/12/2019.
+//  Copyright © 2019 Isioma Anofienam. All rights reserved.
+//
+
+import XCTest
+@testable import Research_Companion
+
+class Research_CompanionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
